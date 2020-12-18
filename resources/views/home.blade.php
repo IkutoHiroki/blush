f2cb339e6061bf5d21f484c1a2101acb47419e06
生藤浩樹 <hiroki.max5109-m_m-@docomo.ne.jp>
2020-12-19 00:08:19 +0900

    インデントや記入ミスの修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container pt-5">
-    @csrf
-    <div class="row justify-content-center">
-        <h1>投稿一覧</h1>
-    </div>
+    <div class="container pt-5">
+        @csrf
+        <div class="row justify-content-center">
+            <h1>投稿一覧</h1>
+        </div>
   
-    <div class="col-md-12 col-auto  pt-5">
-      <ul class="nav nav-tabs nav-justified justify-content-center">
-        <li class="nav-item text-dark">
-            <a class="nav-link list-inline active" href="#msgs"
-                data-toggle="tab" role="tab">新着順</a>
-        </li>
+        <div class="col-md-12 col-auto  pt-5">
+            <ul class="nav nav-tabs nav-justified justify-content-center">
+                <li class="nav-item text-dark">
+                    <a class="nav-link list-inline active" href="#msgs"
+                    data-toggle="tab" role="tab">新着順</a>
+                </li>
 
-        <li class="nav-item list-unstyled list-inline">
-            <a class="nav-link" href="#frms"
-                data-toggle="tab" role="tab">人気順</a>
-        </li>
+                <li class="nav-item list-unstyled list-inline">
+                    <a class="nav-link" href="#frms"
+                    data-toggle="tab" role="tab">人気順</a>
+                </li>
 
-        <li class="nav-item list-unstyled list-inline">
-            <a class="nav-link" href="#frms"
-                data-toggle="tab" role="tab">おすすめ</a>
-        </li>
-      </ul>
-  </div>
+                <li class="nav-item list-unstyled list-inline">
+                    <a class="nav-link" href="#frms"
+                    data-toggle="tab" role="tab">おすすめ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
+
+
 @endsection
