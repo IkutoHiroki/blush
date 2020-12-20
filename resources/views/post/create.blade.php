5d34608b7ee1898ff5547fa87c7d11095649c003
生藤浩樹 <hiroki.max5109-m_m-@docomo.ne.jp>
2020-12-20 21:36:28 +0900

    画像投稿ページの改善

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -2,26 +2,25 @@
 
 @section('content')
 
-<div class="container">
-@csrf
-  <div class="row">
-  <div class="col-6">
-  <form action="/post/store" method="post">
-      <label for="content">投稿する画像を選択する</label>
-      <textarea class="form-control" id="content" name="content" rows="6"></textarea>
-    </div>
+  <div class="container-fluid py-3">
+    <h2>投稿する画像を選択する</h2>
+    @csrf
+    <div class="row">
+      <div class="col-md-6 border"  style="padding:300px;">
+      <form action="/post/store" method="post">
+          <img src="..." class="img-fluid" width="100%" height="400" alt="">
+      </div>
 
-    <div class="col-6">
-  <form action="/post/store" method="post">
-      <label for="content">コメントを記入する <span>(空白も可)</span></label>
-      <textarea class="form-control" id="content" name="content" rows="6"></textarea>
+      <div class="col-md-6 pt-5">
+        <label for="content"><h2>コメントを記入する <span>(空白も可)</span></h2></label>
+        <textarea class="form-control" id="content" name="content" rows="8"></textarea>
+        <div class="pt-5">
+          <button type="submit" class="btn  btn-secondary btn-lg btn-block">投稿する</button>
+        </div>
+      </div>
+      </form>
     </div>
-    
-    <button type="submit" class="btn blue-gradient btn-block">投稿する</button>
-  </form>
-</div>
-  
-</div>
+  </div>
 
 
 @endsection
