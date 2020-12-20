8c945c6e17ff83f976857a96718808fd39dfd3c1
生藤浩樹 <hiroki.max5109-m_m-@docomo.ne.jp>
2020-12-20 21:57:09 +0900

    プロフィール関係の細かい位置の変更

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -6,9 +6,9 @@
 <div class="text-center">
   <p class="h1">マイページ</p>
 </div>
-  @csrf
 
 <div class="container">
+  @csrf
   <div class="row">
     <div class="col-md-6 offset-md-3 pt-5">
       <p class="h2">{{$user->name}}</p>
@@ -33,31 +33,31 @@
 </div>
 
 <div class="container">
-<div class="row">
-  <div class="col-md-9 offset-md-1 pt-5">
-    <p class="h4">自己紹介文</p>
-      <div class="card-body border">
-      </div>
-  </div>
-</div>
-</div>
-
-<div class="container pt-5">
   <div class="row">
-    <div class="col-md-12 col-auto">
-      <ul class="nav nav-tabs nav-justified justify-content-center">
-        <li class="nav-item text-dark">
-          <a class="nav-link list-inline active" href="#msgs"
-              data-toggle="tab" role="tab">今までの投稿一覧</a>
-        </li>
-
-        <li class="nav-item list-unstyled list-inline">
-          <a class="nav-link" href="#frms"
-              data-toggle="tab" role="tab"><i class="far fa-thumbs-up"></i>を押した一覧</a>
-        </li>
-      </ul>
+    <div class="col-md-9 offset-md-1 pt-5">
+      <p class="h4">自己紹介文</p>
+        <div class="card-body border">
+          {{$user->profile}}
+        </div>
     </div>
   </div>
 </div>
 
+  <div class="container-fluid pt-3">
+    <div class="row justify-content-center">
+      <div class="col-12 col-auto  py-5">
+        <ul class="nav nav-tabs nav-justified justify-content-center">
+          <li class="nav-item text-dark">
+            <a class="nav-link list-inline active h3" href="#msgs"
+                data-toggle="tab" role="tab">今までの投稿一覧</a></a>
+          </li>
+          <li class="nav-item list-unstyled list-inline">
+            <a class="nav-link h3" href="#frms"
+                data-toggle="tab" role="tab"><i class="far fa-thumbs-up"></i>を押した投稿一覧
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 @endsection
